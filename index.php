cbeea0dade36f2d169e4b7749f87186e3d2554ec
marleyiam <marleysheredder@gmail.com>
2013-10-24 16:15:22 -0200

    atualizando root da app de novo

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ $app->get('/test', function () use ($app) {
     //phpinfo();
     //printer($_SERVER["SERVER_SOFTWARE"]);
     $mongo = "";
-    if($_SERVER['SERVER_NAME'] == "consultantshub.herokuapp.com"){
+    if($_SERVER['SERVER_NAME'] == "http://polar-lake-2571.herokuapp.com"){
         $mongo = new Mongo('mongodb://marley:v1d4l0k4@paulo.mongohq.com:10004/consultantsDB');
     }else if($_SERVER['SERVER_NAME'] == "localhost"){
         $mongo = new Mongo( 'mongodb://localhost:27017');
