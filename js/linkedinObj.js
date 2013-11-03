50bb51791b657cd487e9e8f2635ba9344515a011
marleyiam <marleysheredder@gmail.com>
2013-11-03 14:55:36 -0200

    implementando linkedinObj from encoded json

---
+++ js/linkedinObj.js
@@ -0,0 +1,54 @@
+linkObj = {
+  "_total": "",
+  "values": [{
+    "_key": "~",
+    "associations": "",
+    "boundAccountTypes": {"_total": ""},
+    "dateOfBirth": {
+      "day": "",
+      "month": "",
+      "year": ""
+    },
+    "educations": {
+      "_total": "",
+      "values": []
+    },
+    "emailAddress": "",
+    "firstName": "",
+    "headline": "",
+    "id": "",
+    "imAccounts": {"_total": ""},
+    "industry": "",
+    "interests": "",
+    "languages": {
+      "_total": "",
+      "values": []
+    },
+    "lastName": "",
+    "location": {
+      "country": {"code": ""},
+      "name": ""
+    },
+    "mainAddress": "",
+    "phoneNumbers": {
+      "_total": "",
+      "values": []
+    },
+    "pictureUrl": "",
+    "positions": {
+      "_total": "",
+      "values": []
+    },
+    "publicProfileUrl": "",
+    "publications": {
+      "_total": "",
+      "values": []
+    },
+    "skills": {
+      "_total": "",
+      "values": []
+    },
+    "summary": "",
+    "twitterAccounts": {"_total": ""}
+  }]
+}
